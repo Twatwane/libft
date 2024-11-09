88a0c5feedb791e23f8aa406aa4e15307ec08207
Antoine Josse <ajosse@student.42.fr>
2024-11-09 10:36:02 +0100

    23/23

---
+++ all_funcs/srcs/calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memcpy.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 08:55:44 by marvin            #+#    #+#             */
+/*   Updated: 2024/11/09 10:04:00 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void* ft_calloc(size_t nitems, size_t size)
+{
+    void* ptr;
+    
+    ptr = malloc(nitems * size);
+    if (ptr == NULL)
+        return NULL;
+    ft_memset(ptr, 0, nitems * size);
+    return ptr;
+}
--- all_funcs/srcs/memcpy.c
+++ all_funcs/srcs/memcpy.c
@@ -3,10 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   memcpy.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajosse <ajosse@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 21:12:32 by ajosse            #+#    #+#             */
-/*   Updated: 2024/11/08 13:29:06 by ajosse           ###   ########.fr       */
+/*   Created: 2024/11/09 08:55:44 by marvin            #+#    #+#             */
+/*   Updated: 2024/11/09 10:04:00 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void *ft_memcpy(void *destination, const void *source, size_t n)
+{
+    unsigned char *dest;
+    const unsigned char *src;
+    size_t i;
+
+    dest = (unsigned char *) destination;
+    src = (unsigned char *) source;
+    i = 0;
+
+    while (i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    return (destination);
+}
